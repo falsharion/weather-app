601ed4e9c76571357105884d40f8ad8ef16ff8ee
falomo sharon <falomosharl@gmail.com>
2022-09-14 20:02:53 +0100

    updates

--- js/index.js
+++ js/index.js
@@ -4,16 +4,27 @@ const cityUpdateName = document.querySelector('.city-name p');
 const weatherInfo = document.querySelector('#des');
 const degreeCel = document.querySelector('#degr');
 let clouds = document.querySelector('.clouds');
- 
+let humid = document.querySelector('.humid');
+let feels= document.querySelector('.feels');
+let pressure = document.querySelector('.pressure');
 
 updateData= (city) => {
     cityUpdateName.textContent = city.name;
     console.log(city)
     weatherInfo.textContent = city.weather[0].description;
-    var temp = city.main.temp
+
+    var temp = city.main.temp;
+    var feelsLike = city.main.feels_like;
+    var humidityV = city.main.humidity;
+    var pressureV = city.main.pressure;
     var day = city.weather[0].icon;
+
     degreeCel.innerHTML = convertC(temp);
+    humid.innerHTML = `${humidityV}%`;
+    feels.innerHTML = convertC(feelsLike);
+    pressure.innerHTML = `${pressureV} hPa`
     dayNight(day);
+
 }
 function convertC(num) {
    let x = num - 273.15
